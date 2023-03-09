9eed3d4a3f5e3716358452a9093950b9030f39c0
chayouhyeon <krece4658@gmail.com>
2023-03-09 13:37:48 +0900

    게시글의 유무 판별

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,16 +1,19 @@
 package com.KoreaIT.java.AM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
-
 		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
+		List<Article> articles = new ArrayList<>();
 
 		while (true) {
+
 			System.out.print("명령어 > ");
 			String command = sc.nextLine();
 
@@ -18,13 +21,17 @@ public class Main {
 				System.out.println("명령어를 입력해주세요");
 				continue;
 			}
-
 			if (command.equals("exit")) {
 				break;
 			}
 
 			if (command.equals("article list")) {
-				System.out.println("게시글이 없습니다");
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+				} else {
+					System.out.println("있던데???");
+				}
+
 			} else if (command.equals("article write")) {
 				int id = lastArticleId + 1;
 				System.out.print("제목 : ");
@@ -32,18 +39,29 @@ public class Main {
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
 
-//				System.out.println(title + " / " + body);
+				Article article = new Article(id, title, body);
+				articles.add(article);
 
 				System.out.printf("%d번글이 생성되었습니다\n", id);
 				lastArticleId++;
-
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
 		}
-
 		System.out.println("==프로그램 끝==");
 
 		sc.close();
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
